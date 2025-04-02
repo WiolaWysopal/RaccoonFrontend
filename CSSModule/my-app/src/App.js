9b96d4f7f7dcecf9c822231dd9ee6aeafd0b1a73
wwysopal <wioletta.wysopal@gmail.com>
2025-04-02 12:39:53 +0200

    Create CSSModule directory

---
+++ CSSModule/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import StyledButton from "./StyledButton";
+
+function App() 
+{
+  return (
+    <div>
+      <h1>CSS Modules in React</h1>
+      <StyledButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ CSSModule/my-app/src/StyledButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./StyledButton.module.css"; // Import CSS Modules
+
+const StyledButton = () => {
+  return <button className={styles.button}>Click on me</button>;
+};
+
+export default StyledButton;
